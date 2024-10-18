7a21ac86a150d40b1011a33e2aea16ce6258151e
Yoriichi <arvyecle89@gmail.com>
2024-10-18 23:48:06 +0800

    Add files via upload

---
+++ Model/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Supplier
+    {
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public string ContactInfo { get; set; }
+        public string Address { get; set; }
+    }
+}
